4b92090c640878706d9bbf98dadbba791edb55ef
joecat6 <j.l.cat.6@gmail>
2019-12-11 19:51:27 -0500

    years to model data

--- projection/visualize.R
+++ projection/visualize.R
@@ -1,8 +1,9 @@
 library(readr)
 library(tidyverse)
+library(lubridate)
 
 #load the clean data
-dat <- read_csv("../data/cleaned/model_data.csv")
+dat1 <- read_csv("../data/cleaned/model_data.csv")
 
 # load the raw data
 dat2 <- read_csv("../data/raw/baseball_data.csv") 
@@ -22,17 +23,74 @@ get.wOBA.value <- function(Y) {
   wOBA.value[Y=="Double"] <- 1.25
   wOBA.value[Y=="Triple"] <- 1.6
   wOBA.value[Y=="Home run"] <- 2
+  return(wOBA.value)
 }
 
 # join the batter id, pitcher id, game_date, and woba values from raw data
 
-dat <- dat %>%
+dat <- data.frame(outcome=dat$outcome) %>%
   mutate(batter=dat2$batter,
          pitcher=dat2$pitcher,
-         game_date=dat2$game_date,
+         xwOBA=as.numeric(dat2$estimated_woba_using_speedangle),
+         game_date=as.Date(dat2$game_date),
          woba_value=get.wOBA.value(outcome),
-         woba_denom=get.wOBA.denom(outcome))
+         woba_denom=get.wOBA.denom(outcome),
+         des=dat2$des) 
 
+dat[is.na(dat$xwOBA),]$xwOBA <- dat[is.na(dat$xwOBA),]$woba_value
+  
+rm(dat1)
 rm(dat2)
 
-dat$rfxwOBA <- rowMeans(rfxwOBA)
+rfx <- readRDS('../data/projection/rfxwOBA_1000.rds')
+
+
+dat$rfxwOBA <- rowMeans(rfx)
+
+# player id data
+
+id.dat <- read_csv("../data/raw/player_id.csv") %>%
+  select(mlb_id, mlb_name)
+
+# function to generate distribtion of rfxwOBA for a given player in a given year
+
+get.rfxwOBA.dist <- function(dat, player.name, id.dat, yr, type, rfx) {
+  id <- id.dat$mlb_id[id.dat$mlb_name==player.name]
+  if (type=="batter") {
+    idx <- which(dat$batter==id & year(dat$game_date)==yr)
+  } else if (type=="pitcher") {
+    idx <- which(dat$pitcher==id & year(dat$game_date)==yr)
+  }
+  player.df <- dat[idx,] %>% select(woba_value, woba_denom, xwOBA, rfxwOBA)
+  rfx.df <- rfx[idx,] %>% 
+    colSums() %>%
+    as.data.frame() 
+  colnames(rfx.df) <- "rfxwOBA"
+  rfx.df %>% ggplot(aes(x=rfxwOBA/sum(player.df$woba_denom))) + 
+    geom_density() + 
+    geom_vline(aes(xintercept=sum(player.df$woba_value)/sum(player.df$woba_denom), color="Actual wOBA")) +
+    geom_vline(aes(xintercept=sum(player.df$rfxwOBA)/sum(player.df$woba_denom), color="Mean rfxwOBA")) +
+    geom_vline(aes(xintercept=sum(player.df$xwOBA, na.rm=T)/sum(player.df$woba_denom), color="Baseball Savant xwOBA")) +
+    labs(title=paste0(player.name, " rfxwOBA Disribution - ", yr),
+         x="rfxwOBA", color="Statistic")
+}
+
+# function for 3 year rolling wOBA vs estimates
+
+rolling.rfxwOBA <- function(dat, player.name, id.dat, type) {
+  id <- id.dat$mlb_id[id.dat$mlb_name==player.name]
+  if (type=="batter") {
+    idx <- which(dat$batter==id)
+  } else if (type=="pitcher") {
+    idx <- which(dat$pitcher==id)
+  }
+  player.df <- dat[idx,] %>% 
+    select(woba_value, woba_denom, xwOBA, rfxwOBA, game_date) %>%
+    arrange(game_date)
+  player.df %>% ggplot(aes(x=game_date)) +
+    geom_line(aes(y=cumsum(woba_value)/cumsum(woba_denom), color="Actual wOBA")) +
+    geom_line(aes(y=cumsum(rfxwOBA)/cumsum(woba_denom), color="Mean rfxwOBA")) +
+    geom_line(aes(y=cumsum(xwOBA)/cumsum(woba_denom), color="Baseball Savant xwOBA")) +
+    labs(title=paste0(player.name, " rolling wOBA vs. estimates: 2017-2019"),
+         x="Date", y="Value", color="Statistic")
+}
